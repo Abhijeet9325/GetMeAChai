89c1a8bc19641f9156f752e9d282b267149c84bf
Abhijit Wankhade <abhijeetwankhade816@gmail.com>
2026-02-02 16:25:58 +0530

    feat(payments): implement Razorpay test mode payment flow

---
+++ app/api/razorpay/routes.jsx
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils";
+import Razorpay from "razorpay";
+import Payment from "@/app/models/Payment";
+import connectDB from "@/db/connectDB";
+
+export const POST = async (req) => {
+    await connectDB();
+    let body = await req.formData();
+    body = Object.fromEntries(body);
+    // check if razorpayOrderId is present on the server
+    let p = await Payment.findOne({ oid: body.razorpay_order_id })
+    if (!p) {
+        return NextResponse.json({ success: false , message :"order id not found"})
+    }
+
+    // verify the payment
+    let xx = validatePaymentVerification({ "order_id": body.razorpay_order_id, "payment_id": body.razorpay_payment_id }, body.razorpay_signature, process.env.NEXT_PUBLIC_RAZORPAY_SECRET_ID)
+
+    if (xx) {
+        const updatePayment = await Payment.findOneAndUpdate({ oid: body.razorpay_order_id }, { done: "true" }, { new: true })
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_RAZORPAY_URL}/${updatePayment.to_user}?paymentdone=true`)
+    }
+    else {
+        return NextResponse.json({ success: false , message :"payment verifcation failed"})
+    }
+
+}
